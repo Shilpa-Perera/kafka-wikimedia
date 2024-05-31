c75ae822b3b9b5b560246626d9e10dde0c4a8dd5
Shilpa-Perera <laksahaniperera@gmail.com>
2024-06-01 01:17:17 +0530

    initial commit

---
+++ src/test/java/com/learn/kafka_wikimedia/KafkaWikimediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.kafka_wikimedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaWikimediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
